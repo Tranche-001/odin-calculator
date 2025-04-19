556033cf157f70a650ed21ed765b7cfa1a04b61c
Tranche-001 <tranche100@gmail.com>
2025-04-19 17:01:46 -0300

    add some button logic, when it is pressed show to the display

--- script.js
+++ script.js
@@ -32,7 +32,55 @@ function operate(operator, a, b){
   }
 }
 
-console.log(add(2,3))
-console.log(subtract(2,3))
-console.log(multiply(2,3))
-console.log(divide(2,3))
+let displayNumber = 0;
+
+const display = document.querySelector("#display")
+display.textContent = "banana"
+const btn0 = document.querySelector("#button0")
+const btn1 = document.querySelector("#button1");
+const btn2 = document.querySelector("#button2");
+const btn3 = document.querySelector("#button3");
+const btn4 = document.querySelector("#button4");
+const btn5 = document.querySelector("#button5");
+const btn6 = document.querySelector("#button6");
+const btn7 = document.querySelector("#button7");
+const btn8 = document.querySelector("#button8");
+const btn9 = document.querySelector("#button9");
+//Buttons Listeners
+btn1.addEventListener("click", (e) =>{
+  displayNumber = 1;
+  display.textContent = displayNumber;
+})
+btn2.addEventListener("click", (e) =>{
+  displayNumber = 2;
+  display.textContent = displayNumber;
+})
+btn3.addEventListener("click", (e) =>{
+  displayNumber = 3;
+  display.textContent = displayNumber;
+})
+btn4.addEventListener("click", (e) => {
+  displayNumber = 4;
+  display.textContent = displayNumber;
+})
+btn5.addEventListener("click", (e) =>{
+  displayNumber = 5;
+  display.textContent = displayNumber;
+})
+btn6.addEventListener("click", (e) =>{
+  displayNumber = 6;
+  display.textContent = displayNumber;
+})
+btn7.addEventListener("click", (e) =>{
+  displayNumber = 7;
+  display.textContent = displayNumber;
+})
+btn8.addEventListener("click", (e) =>{
+  displayNumber = 8;
+  display.textContent = displayNumber;
+})
+btn9.addEventListener("click", (e) =>{
+  displayNumber = 9;
+  display.textContent = displayNumber;
+})
+
