c1a65227144e9b16f879d9077a0b373f86fe3670
Tranche-001 <tranche100@gmail.com>
2025-04-21 18:56:20 -0300

    Creating Buttons Objects for the DOM
    change the buttons from the html for objects
    created in the JS. In that way the code gets
    more concise and easier to manipulate.

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+const upperNumbers = document.querySelector(".upper-numbers");
+const middleNumbers = document.querySelector(".middle-numbers");
+const bottomNumbers = document.querySelector(".bottom-numbers");
+const number0AndDot = document.querySelector(".number0-dot");
+//Creating Buttons
+function createNumberButtons(){
+  //There are 11 numbers in the numbers section
+  for(let i=0; i<11; i++){
+    let numberBtn = document.createElement("button");
+    numberBtn.id = `button${i}`;
+    numberBtn.classList.add("number");
+    numberBtn.classList.add("unit");
+    if(i==0){
+      numberBtn.classList.replace("number", "number0");
+      numberBtn.textContent = '0';
+      number0AndDot.appendChild(numberBtn);
+    } 
+    else if(i < 4){
+      numberBtn.textContent = `${i}`;
+      bottomNumbers.appendChild(numberBtn);
+    } 
+    else if(i < 7){
+      numberBtn.textContent = `${i}`;
+      middleNumbers.appendChild(numberBtn);
+    } 
+    else if(i < 10){
+      numberBtn.textContent = `${i}`;
+      upperNumbers.appendChild(numberBtn);
+    }
+    else {
+      numberBtn.classList.replace("number", "number-dot");
+      numberBtn.textContent = '.';
+      number0AndDot.appendChild(numberBtn);
+    }
+      
+  }
+
+}
+createNumberButtons();
+
+// Uma vez que os botões estão criados
+// O próximo passo é configurar o funcionamento deles.
+
+//----- BUTTON FUNCTIONS -----------
+
+let numberButtons = document.querySelectorAll(".unit");
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 function add(a, b) {
   return a + b;
 }
@@ -32,76 +96,92 @@ function operate(operator, a, b){
   }
 }
 
-let displayNumber = 0;
+// let displayNumber='';
 
-const display = document.querySelector("#display-content")
-display.textContent = "banana"
-const btn0 = document.querySelector("#button0");
-const btn1 = document.querySelector("#button1");
-const btn2 = document.querySelector("#button2");
-const btn3 = document.querySelector("#button3");
-const btn4 = document.querySelector("#button4");
-const btn5 = document.querySelector("#button5");
-const btn6 = document.querySelector("#button6");
-const btn7 = document.querySelector("#button7");
-const btn8 = document.querySelector("#button8");
-const btn9 = document.querySelector("#button9");
-const btnAdd = document.querySelector("#add");
-const btnSub = document.querySelector("#subtract");
-const btnDivide = document.querySelector("#divide");
-const btnMultiply = document.querySelector("#multiply");
-const btnEqualSign = document.querySelector("#equal-sign");
+// const display = document.querySelector("#display-content")
+// display.textContent = ""
 
+// const btn9 = document.querySelector("#button9");
+// const btnAdd = document.querySelector("#add");
+// const btnSub = document.querySelector("#subtract");
+// const btnDivide = document.querySelector("#divide");
+// const btnMultiply = document.querySelector("#multiply");
+// const btnEqualSign = document.querySelector("#equal-sign");
 
+//Number Saving Logic
+let numbers = {
+  a: null,
+  b: null
+}
 
-
-//Buttons Listeners
-btn0.addEventListener("click", (e) =>{
-  displayNumber = 0;
-  display.textContent = displayNumber;
-})
-btn1.addEventListener("click", (e) =>{
-  displayNumber = 1;
-  display.textContent = displayNumber;
-})
-btn2.addEventListener("click", (e) =>{
-  displayNumber = 2;
-  display.textContent = displayNumber;
-})
-btn3.addEventListener("click", (e) =>{
-  displayNumber = 3;
-  display.textContent = displayNumber;
-})
-btn4.addEventListener("click", (e) => {
-  displayNumber = 4;
-  display.textContent = displayNumber;
-})
-btn5.addEventListener("click", (e) =>{
-  displayNumber = 5;
-  display.textContent = displayNumber;
-})
-btn6.addEventListener("click", (e) =>{
-  displayNumber = 6;
-  display.textContent = displayNumber;
-})
-btn7.addEventListener("click", (e) =>{
-  displayNumber = 7;
-  display.textContent = displayNumber;
-})
-btn8.addEventListener("click", (e) =>{
-  displayNumber = 8;
-  display.textContent = displayNumber;
-})
-btn9.addEventListener("click", (e) =>{
-  displayNumber = 9;
-  display.textContent = displayNumber;
-})
+function saveNumbers(numbers, number){
+  if(!numbers.a){
+    numbers.a = number;
+  }
+  else{
+    numbers.b = number;
+  }
+}
 
 
-btnAdd.addEventListener("click", (e) => {
-  operator = '+';
-})
 
-btnEqualSign.addEventListener("click", (e) => {
-  operate(operator, displayNumber, )
-})
+
+
+
+
+// btn0.addEventListener("click", (e) =>{
+//   displayNumber += '0';
+//   display.textContent = displayNumber;
+// })
+// btn1.addEventListener("click", (e) =>{
+//   displayNumber += '1';
+//   display.textContent = displayNumber;
+// })
+// btn2.addEventListener("click", (e) =>{
+//   displayNumber += '2';
+//   display.textContent = displayNumber;
+// })
+// btn3.addEventListener("click", (e) =>{
+//   displayNumber += '3';
+//   display.textContent = displayNumber;
+// })
+// btn4.addEventListener("click", (e) => {
+//   displayNumber += '4';
+//   display.textContent = displayNumber;
+// })
+// btn5.addEventListener("click", (e) =>{
+//   displayNumber += '5';
+//   display.textContent = displayNumber;
+// })
+// btn6.addEventListener("click", (e) =>{
+//   displayNumber += '6';
+//   display.textContent = displayNumber;
+// })
+// btn7.addEventListener("click", (e) =>{
+//   displayNumber += '7';
+//   display.textContent = displayNumber;
+// })
+// btn8.addEventListener("click", (e) =>{
+//   displayNumber += '8';
+//   display.textContent = displayNumber;
+// })
+// btn9.addEventListener("click", (e) =>{
+//   displayNumber += '9';
+//   display.textContent = displayNumber;
+// })
+
+
+// // +
+// // -clean no display
+
+
+// btnAdd.addEventListener("click", (e) => {
+//   saveNumbers(numbers, displayNumber);
+//   operator = '+';
+  
+  
+// })
+
+// btnEqualSign.addEventListener("click", (e) => {
+//   operate(operator, displayNumber, )
+// })
